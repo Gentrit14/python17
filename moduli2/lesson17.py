9fcd5d9caa80cd09eed623a06df002b4bf838889
Student <123@gmail.com>
2025-04-05 03:34:09 -0700

    ...

--- moduli2/lesson17.py
+++ moduli2/lesson17.py
@@ -1,7 +1,10 @@
+from operator import index
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 from numpy.ma.extras import unique, average
+from select import select
 
 # col1,col2,col3, col4 , col5 = st.columns (gap=5 , gap="small")
 #
@@ -72,6 +75,51 @@ books_df = pd.read_csv('moduli2/bestsellers_with_categories_2022_03_27.csv')
 
 st.title('Best selling Books Analysis')
 st.write('This app analyzes the Amazon top selling books.')
+
+st.sidebar.header("Add New Book Data")
+with st.sidebar.form("book_form"):
+    new_name = st.text_input("book name")
+    new_author = st.text_input("Author name")
+    new_user_rating = st.slider("User Rating", 0.5,5.0, 0.0, 0.1)
+    new_review = st.number_input('Review', min_value=0, step=1)
+    new_price = st.number_input('Price', min_value=0, step=1)
+    new_year = st.number_input('Year', min_value=2009, max_value=2022)
+    new_Genre = st.selectbox("Genre", books_df['Genre'].unique())
+    submit_button = st.form_submit_button(label="Add Book")
+
+if submit_button:
+    new_data = {
+        'Name': new_name,
+        'Author': new_author,
+        'User Rating': new_user_rating,
+        'Reviews': new_review,
+        'Price': new_price,
+        'Year': new_year,
+        'Genre': new_Genre
+    }
+    books_df = pd.concat([pd.DataFrame(new_data, index=[0]), books_df], ignore_index=True)
+    books_df.to_csv('moduli2/bestsellers_with_categories_2022_03_27.csv', index=False)
+    st.sidebar.success("New Book Added successfully")
+
+st.sidebar.header("filter Options")
+selected_author = st.sidebar.selectbox("Select Author", ['All'] +list(books_df['Author'].unique()))
+selected_Year = st.sidebar.selectbox("Select Year", ['All'] +list(books_df['Year'].unique()))
+selected_genre = st.sidebar.selectbox("Select Genre", ['All'] +list(books_df['Genre'].unique()))
+min_rating = st.sidebar.slider("Minimum User Rating", 0.5,5.0,0.0,0.1)
+max_price = st.sidebar.slider("Maximum Price", 0, books_df['Price'].max(), books_df['Price'].max())
+
+filtered_Book_df = books_df.copy()
+
+if selected_author != "All":
+    filtered_Book_df = filtered_Book_df[filtered_Book_df['Author'] == selected_author]
+if selected_Year != "All":
+    filtered_Book_df = filtered_Book_df[filtered_Book_df['Year'] == selected_Year]
+if selected_genre != "All":
+    filtered_Book_df = filtered_Book_df[filtered_Book_df['Genre'] == selected_genre]
+
+filtered_Book_df = filtered_Book_df[( filtered_Book_df['User Rating'] >= min_rating) & filtered_Book_df['Price'] <= max_price]
+
+
 
 st.subheader('Summary List')
 total_books = books_df.shape[0]
